26bd9c274b7f32a4a91059e015bfa138fedbccec
Kha Đỗ Văn <dovankha.tlvn@gmail.com>
2020-08-21 18:00:49 +0700

    Kha dang test chuc nang VS code

--- TheLastCode.cpp
+++ TheLastCode.cpp
@@ -1,59 +1,82 @@
-#include<iostream>
-#include<math.h>
+#include <iostream>
+#include <math.h>
 
 using namespace std;
 
 // ham: funtion -> chuc nang: lam nhiem vu cua cai ten ham. Chi co chuc nang xi li phan ruot.
-void DemChuSoCuaN(int n)
+int bai1(int n)
 {
-	int dem=0,k=n;
-	while ( n != 0 )
+	int dem = 0;
+	while (n > 0)
 	{
 		n /= 10;
 		dem++;
 	}
-	n=k;
-	cout<<k<<" co "<<dem<<" chu so";
+	return dem;
+}
+
+int bai2(int n)
+{
+	int sum = 0, h;
+	while (n > 0)
+	{
+		h = n % 10;
+		sum += h;
+		n /= 10;
+	}
+	return sum;
+}
+
+void DemChuSoCuaN(int n)
+{
+	int dem = 0, k = n;
+	while (n != 0)
+	{
+		n /= 10;
+		dem++;
+	}
+	n = k;
+	cout << k << " co " << dem << " chu so";
 }
 
 void TongChuSoChan(int n)
 {
-	int s=0,k=n;
-	while ( n != 0 )
+	int s = 0, k = n;
+	while (n != 0)
 	{
-		if ((n%10)%2==0) s+=n%10;
+		if ((n % 10) % 2 == 0)
+			s += n % 10;
 		n /= 10;
 	}
-	cout<<"Tong cac chu so cua "<<k<<" la: "<<s;
+	cout << "Tong cac chu so cua " << k << " la: " << s;
 }
 
 int kTNguyenTo(int n)
 {
-	if(n<2)
+	if (n < 2)
 		return 0;
-	else 
+	else
 	{
-		for (int i=2;i<=sqrt(n);++i)
+		for (int i = 2; i <= sqrt(n); ++i)
 		{
-			if(n%i==0)
+			if (n % i == 0)
 				return 0;
 		}
-		
 	}
 	return 1;
 }
 
 void lietKeSnt(int &n)
 {
-	int t=0;
-	for(int i=0;i<n*10;++i)
+	int t = 0;
+	for (int i = 0; i < n * 10; ++i)
 	{
-		if(kTNguyenTo(i)==1)
+		if (kTNguyenTo(i) == 1)
 		{
-			cout<<i<<" ";
+			cout << i << " ";
 			t++;
 		}
-		if(t==n)	
+		if (t == n)
 			break;
 	}
 }
@@ -86,66 +109,67 @@ int timChuSoNhoNhat(int n)
 
 int xuLi7(int n)
 {
-	int min=9,count=1; 
-	while (n!=0)
+	int min = 9, count = 1;
+	while (n != 0)
 	{
-		if ((n%10)<=min)
+		if ((n % 10) <= min)
 		{
-			int temp=min; 
-			min=n%10;
-			if (temp==min)
+			int temp = min;
+			min = n % 10;
+			if (temp == min)
 				count++;
-				else count=1; 
+			else
+				count = 1;
 		}
-		n/=10;	 
-	} 
-	return count; 
-}  
+		n /= 10;
+	}
+	return count;
+}
 
 bool xuLi8(int n)
 {
-	while (n!=0)
+	while (n != 0)
 	{
-		if ((n%10)%2!=0) 
+		if ((n % 10) % 2 != 0)
 			return false;
-		n/=10;
+		n /= 10;
 	}
-	return true; 
-} 
+	return true;
+}
 
 int main()
 {
-	int n,k;
-	cout<<"Nhap N: ";
-	cin>>n;
-	// bai 1, 2 cua Toan gia la nam rieng ben kia nha ae oi 
+	int n, k;
+	cout << "Nhap N: ";
+	cin >> n;
+	// Toan **********************************
+	cout << "\nBai 1 - So luong chu so cua " << n << " la:" << bai1(n) << endl;
 	//Trung ....................................
-	cout<<"Bai 3- ";
+	cout << "Bai 3- ";
 	DemChuSoCuaN(n);
-	cout<<"\nBai 4- ";
+	cout << "\nBai 4- ";
 	TongChuSoChan(n);
-	
+
 	//Kha dep trai ----------
 	cout << "\nBai 5 - Chu so dau tien cua " << n << " la: " << timChuSoFirst(n) << endl;
 	cout << "Bai 6 - Chu so nho nhat cua " << n << " la: " << timChuSoNhoNhat(n) << endl;
-	//Hoat---------------------- 
-	cout<<"Bai 7 - So luong chu so nho nhat cua n la: "<<xuLi7(n)<<endl;
-	
-	if (xuLi8(n)==true)
-		cout<<"Bai 8 - n toan la chu so chan"<<endl;
-	else 
-		cout<<"Bai 8 - n co chu so le"<<endl; 
-	
+	//Hoat----------------------
+	cout << "Bai 7 - So luong chu so nho nhat cua n la: " << xuLi7(n) << endl;
+
+	if (xuLi8(n) == true)
+		cout << "Bai 8 - n toan la chu so chan" << endl;
+	else
+		cout << "Bai 8 - n co chu so le" << endl;
+
 	//vuong ....................................
-	cout<<"\nBai 9: Cac SNT Tu 2 den N la: ";
-	for(int i=0;i<n;++i)
+	cout << "Bai 9: Cac SNT Tu 2 den N la: ";
+	for (int i = 0; i < n; ++i)
 	{
-		if(kTNguyenTo(i)==1)
-			cout<<i<<" ";
+		if (kTNguyenTo(i) == 1)
+			cout << i << " ";
 	}
-	cout<<"\nBai 10: N SNT dau tien la: ";
+	cout << "\nBai 10: N SNT dau tien la: ";
 	lietKeSnt(n);
 	//..........................................
 	return 0;
 }
-
--- bai1.cpp
+++ bai1.cpp
@@ -1,24 +1,27 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 int main()
 {
-	int n , h ;
-	int dem=0 ;
-	cout<<"Nhap n :";
-	cin>>n;
-	h=n;
-	while(n>0)
+	int n, h;
+	int dem = 0;
+	cout << "Nhap n :";
+	cin >> n;
+	h = n;
+	while (n > 0)
 	{
-		n=n/10;
+		n = n / 10;
 		dem++;
 	}
-	cout<<"So luong chu so cua "<<h<<" la :"<<dem;
+	if (n > 0)
+		cout << "Kha dep trai nhat" << endl;
+	else
+		cout << "Kha van dep trai nhat" << endl;
+
+	cout << "So luong chu so cua " << h << " la :" << dem;
 	cout << "\nKha dang test chuc nang tren visual studio code 2019" << endl;
-<<<<<<< HEAD
-	cout << "\nKha da test xong." << endl;
-=======
 	cout << "\nAnh em ma dung git tren visual studio no phe lam nha anh em oi" << endl;
->>>>>>> 5bb6d6b370257e38233dfd48fd25ea0854b6644a
+	cout << "\nKha dep trai nhat qua dat ne anh em oi" << endl;
+	system("pause");
 	return 0;
 }
